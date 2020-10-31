d4e9980c6045a8f18423b79804fdff03ebdd58d8
DeVivres <60067346+DeVivres@users.noreply.github.com>
2020-10-31 11:27:49 +0200

    Again hosting fix

--- src/components/ProductTile/ProductTile.js
+++ src/components/ProductTile/ProductTile.js
@@ -9,54 +9,21 @@ class ProductTile extends Component {
                 img: Image,
                 text: String,
                 price: String,
-                isFullCover: Boolean,
-                isButton: Boolean,
             }
         }
     }
     render() {
-        const isCover = this.state.card.isFullCover;
-        const hasButton = this.state.card.isButton;
-        let card;
-        if(isCover == true) {
-            card = 
-            <div>
-                <div class="card text-center">
-                    <img class="card-img-top" src={this.props.card.img}></img>
-                </div>
-            </div>
-        }
-        else if(hasButton == true){
-            card = 
-            <div>
-                <div class="card text-center">
-                    <img class="card-img-top" src={this.props.card.img}></img>
-                    <div class="card-body">
-                        <p class="card-text">{this.props.card.text}</p>
-                        <p>{this.props.card.price}</p>
-                        <button>Press here</button>
-                    </div>
-                </div>
-            </div>
-        }
-        else{
-            card = 
-            <div>
-                <div class="card text-center">
-                    <img class="card-img-top" src={this.props.card.img}></img>
-                    <div class="card-body">
-                        <p class="card-text">{this.props.card.text}</p>
-                        <p>{this.props.card.price}</p>
-                    </div>
-                </div>
-            </div>
-        }
-        
         return (
             <div>
-                {card}
+                <div class="card text-center">
+                    <img class="card-img-top" src={this.props.card.img}></img>
+                    <div class="card-body">
+                        <p class="card-text">{this.props.card.text}</p>
+                        <p>{this.props.card.price}</p>
+                    </div>
+                </div>
             </div>
-        );
+        )      
     }
 }
 
