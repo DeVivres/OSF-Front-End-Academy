31ce17de80d796ed68ede792374cc4bb2a61e810
DeVivres <60067346+DeVivres@users.noreply.github.com>
2020-10-31 10:24:32 +0200

    Updates

---
+++ precache-manifest.79722e6483fa4a07e0e19c9a3e3a8eb0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7ab324a72ae9be4b12a444ba8386d7",
+    "url": "/OSF-Front-End-Academy/index.html"
+  },
+  {
+    "revision": "f5a545ce34437c53dd2a",
+    "url": "/OSF-Front-End-Academy/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "e7afff6691d57beeb829",
+    "url": "/OSF-Front-End-Academy/static/css/main.610f5ed3.chunk.css"
+  },
+  {
+    "revision": "f5a545ce34437c53dd2a",
+    "url": "/OSF-Front-End-Academy/static/js/2.fbaadf4b.chunk.js"
+  },
+  {
+    "revision": "b8304e834329e93c4a4301016544d028",
+    "url": "/OSF-Front-End-Academy/static/js/2.fbaadf4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7afff6691d57beeb829",
+    "url": "/OSF-Front-End-Academy/static/js/main.57016c54.chunk.js"
+  },
+  {
+    "revision": "c7596df4352fea058a5a",
+    "url": "/OSF-Front-End-Academy/static/js/runtime-main.7cdf3c19.js"
+  },
+  {
+    "revision": "93917a4b12791650d3a01a750a076882",
+    "url": "/OSF-Front-End-Academy/static/media/Carousel_Photo1.93917a4b.png"
+  },
+  {
+    "revision": "462b0d96cf4a002e336cfff7bfe85f7b",
+    "url": "/OSF-Front-End-Academy/static/media/Carousel_Photo2.462b0d96.png"
+  },
+  {
+    "revision": "3f78e7dbe75448e3ee9fc80489b767ee",
+    "url": "/OSF-Front-End-Academy/static/media/Carousel_Photo3.3f78e7db.png"
+  },
+  {
+    "revision": "cb6ec5db987981b39f36ec61af2d5f70",
+    "url": "/OSF-Front-End-Academy/static/media/FeaturedProducts_Image1.cb6ec5db.png"
+  },
+  {
+    "revision": "b86dd2109ee7d044078e073858462ed1",
+    "url": "/OSF-Front-End-Academy/static/media/FeaturedProducts_Image2.b86dd210.png"
+  },
+  {
+    "revision": "71242f3c110e7d919046838d2a714c71",
+    "url": "/OSF-Front-End-Academy/static/media/FeaturedProducts_Image3.71242f3c.png"
+  },
+  {
+    "revision": "4b7676f9d95491d1864e0c513d367cbe",
+    "url": "/OSF-Front-End-Academy/static/media/FeaturedProducts_Image4.4b7676f9.png"
+  },
+  {
+    "revision": "b27a9805a970707de60d09f19a2d447a",
+    "url": "/OSF-Front-End-Academy/static/media/FullWidthImage_Banner_Desktop.b27a9805.png"
+  },
+  {
+    "revision": "c646af40f05fc5af9641de4710194bd6",
+    "url": "/OSF-Front-End-Academy/static/media/Mobile_Photo_BurgerMeny.c646af40.png"
+  },
+  {
+    "revision": "d3338bcbd3a74e512bf5b78b8468e24d",
+    "url": "/OSF-Front-End-Academy/static/media/OSF_Logo.d3338bcb.png"
+  },
+  {
+    "revision": "ad6e2b1ebe21536b412dbe2b9ffc3a94",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image1.ad6e2b1e.png"
+  },
+  {
+    "revision": "a6ca0443fff6e94895d2842b33fc5c9f",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image2.a6ca0443.png"
+  },
+  {
+    "revision": "2401ef9bea620c229a631a69037ddf43",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image3.2401ef9b.png"
+  },
+  {
+    "revision": "7e9743e3a44003cecd3f1dd184a617d8",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image4.7e9743e3.png"
+  },
+  {
+    "revision": "db3b0067105c6800ef968fe063f0e5a1",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image5.db3b0067.png"
+  },
+  {
+    "revision": "91ddc0aa3ec7a65f146b4e4c8a712d46",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image6.91ddc0aa.png"
+  },
+  {
+    "revision": "ad4dc6e554273fd98b8ee6165fe323ec",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image7.ad4dc6e5.png"
+  },
+  {
+    "revision": "8ab89bb1fb765df9b6c1e57761382a8e",
+    "url": "/OSF-Front-End-Academy/static/media/PopularItems_Image8.8ab89bb1.png"
+  },
+  {
+    "revision": "5610df49cf356bc3f6930ce9d1118b9d",
+    "url": "/OSF-Front-End-Academy/static/media/Sale_Image.5610df49.png"
+  }
+]);
